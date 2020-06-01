25ccbfd90ceb7be328d5d10863669ecb333c5ae5
webb.shi <bradyjoestar@gmail.com>
2020-06-01 11:54:48 +0800

    add bugfix

--- core/main_test.go
+++ core/main_test.go
@@ -1,7 +1,15 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestHelloWorld(t *testing.T) {
 	t.Log("hello world")
+}
+
+func TestBugfix(t *testing.T) {
+	fmt.Println("this is a test for bugfix")
+	t.Log("bug fix")
 }
